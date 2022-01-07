f4bb9513783ebd95b224cb6af2644d1e1fc5e2ab
Diego Quintela <diegolucasq@gmail.com>
2022-01-07 09:53:09 -0500

    added new folders for components; set transaction functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
+import Expenses from "./components/Expenses/Expenses";
+
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: 0.3,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e2",
+      title: "Car Insurance",
+      amount: 0.3,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 0.3,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e4",
+      title: "Car Insurance",
+      amount: 0.3,
+      date: new Date(2020, 7, 14),
+    },
+  ];
+
+  // const expenseItemMap = expenses.map((i) => (
+  //   <ExpenseItem
+  //     title={expenses[i].title}
+  //     amount={expenses[i].amount}
+  //     date={expenses[i].date}
+  //   />
+  // ));
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
