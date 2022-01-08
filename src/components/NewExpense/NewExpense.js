4b33b4828cb476e76fc1a06250a2d3fd0a1db16f
Diego Quintela <diegolucasq@gmail.com>
2022-01-07 19:02:51 -0500

    added new filter component for selecting years and sorting expenses

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,22 @@
+import ExpenseForm from "./ExpenseForm";
+
+import "./NewExpense.css";
+
+const NewExpense = (props) => {
+  const onSaveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    //sending data to parent app.js
+    props.onAddExpense();
+  };
+
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} />
+    </div>
+  );
+};
+
+export default NewExpense;
